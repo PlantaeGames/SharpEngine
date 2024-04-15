5bf6c6b9366dc923d1e2a769248f2f156b363e1e
PlantaeGames <plantaegames@gmail.com>
2024-04-15 16:40:10 +0500

    window test,

---
+++ SharpEngineCore/Components/App.cs
@@ -0,0 +1,42 @@
+﻿namespace SharpEngineCore.Components;
+
+internal class App
+{
+    private const string NAME = "Sharp Engine";
+
+    private class AppContext : ApplicationContext
+    {
+        public AppContext()
+        {
+            Application.ApplicationExit += OnExit;
+
+            var window = new Form();
+            window.Text = NAME;
+
+            window.FormClosed += OnWindowClosed;
+
+            window.Show();
+        }
+
+        private void OnWindowClosed(object? sender, FormClosedEventArgs e)
+        {
+
+            Console.WriteLine("LOG: Window Closed.");
+
+            ExitThread();
+        }
+
+        private void OnExit(object? sender, EventArgs e)
+        {
+            Console.WriteLine("LOG: Application Exited.");
+        }
+    }
+
+    public ApplicationContext Context => _context;
+    private AppContext _context;
+
+    public App()
+    {
+        _context = new AppContext();
+    }
+}
--- SharpEngineCore/Program.cs
+++ SharpEngineCore/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using SharpEngineCore.Components;
+
+var app = new App();
+var context = app.Context;
+
+Application.Run(context);
